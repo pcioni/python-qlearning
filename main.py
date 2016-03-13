1e962e491703f51653ebfe18d621b178048fd0c6
alarosa <larosa2@rpi.edu>
2016-03-13 12:43:52 -0400

    Added Prebuilt maps, Map building function, agent implementation, move implementation, BASIC qval array, Python 2.x/3.x compatability, Draw loop, End Tile limited stuff

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-from tkinter import *
+try:
+    from Tkinter import *
+except ImportError:
+    from tkinter import *
 import random
 
 class MapDraw:
     Colors = {
         "White"      : "#FFFFFF",
         "LightGreen" : "#9BF04D",
-        "Black"      : "#000000"
+        "Black"      : "#000000",
+        "Red"        : "#FF0000"
     }
 
-    fillColors = {0: "White", 1: "Black", 2: "LightGreen"}
+    fillColors = {0: "White", 1: "Black", 2: "LightGreen", 3: "Red"}
 
     def __init__(self, screenWidth, screenHeight, cellX, cellY):
         self.width = screenWidth
@@ -63,10 +67,79 @@ class generateDungeon:
     def setTile(self, x, y, value):
         self.cellMap[x][y] = value
 
+    def buildMap(self, rand, mapin):
+        if (rand == True):
+            for i in range(self.cell_y):
+                for j in range(self.cell_x):
+                    self.cellMap[i][j] = random.randint(0,1)
+        else:
+            for i in range(self.cell_y):
+                for j in range(self.cell_x):
+                    self.cellMap[i][j] = mapin[i][j]
+
+    def tileCheck(self, x, y):
+        if ( (x < 0) or (x >= self.cell_x) or (y < 0) or (y >= self.cell_y) or (self.cellMap[x][y] == 1)):
+            return False
+        else:
+            return True
+
+class Agent:
+    Dirs = {
+        "up"    : 0,
+        "down"  : 1,
+        "left"  : 2,
+        "right" : 3
+    }
+
+    def __init__(self, startX, startY, size):
+        self.xPos = startX
+        self.yPos = startY
+        self.Qvals = [[0 for i in range(size)] for j in range(4)]    #we have Q values for every space, even if we dont use it
+
+    def Move(self, dir, mg):
+        dx = 0
+        dy = 0
+        if (dir == "up"):
+            dx = -1
+        elif (dir == "down"):
+            dx = 1
+        elif (dir == "left"):
+            dy = -1
+        elif (dir == "right"):
+            dy = 1
+        else:
+            print("ERROR: not a direction")
+
+        if (mg.tileCheck(self.xPos+dx, self.yPos+dy) ):
+            mg.setTile(self.xPos, self.yPos, 0);
+            self.xPos += dx
+            self.yPos += dy
+            mg.setTile(self.xPos, self.yPos, 2);
+            return True;
+        else:
+            return False;
+
+map1 = [ [0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0] ]
+
+map2 = [ [0,0,0,0,0,0,0,0,0,0], [1,1,1,1,1,1,1,1,1,0], [0,0,0,0,0,0,0,0,0,0], [0,1,1,1,1,1,1,1,1,1], [0,0,0,0,0,0,0,0,0,0], [1,1,1,1,1,1,1,1,1,0], [0,0,0,0,0,0,0,0,0,0], [0,1,1,1,1,1,1,1,1,1], [0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,3] ]
+
+map3 = [ [0,1,0,0,1,1,1,1,0,0], [0,0,0,0,0,1,1,1,1,0], [0,1,0,1,1,1,1,0,0,1], [0,1,0,0,0,0,0,0,0,0], [1,0,0,0,0,0,0,0,1,0], [1,1,0,1,0,1,1,1,1,0], [1,1,0,1,0,0,0,0,1,1], [1,0,0,0,0,0,0,1,1,0], [0,0,0,1,1,0,0,0,0,0], [0,0,1,1,1,1,0,0,0,3] ]
+
 #The last two variables in MapDraw determine how many squares make up a grid;
 # for some reason, if they aren't equal, things break. Who cares though, right?
 GFX = MapDraw(600,600,10,10)
 MG = generateDungeon(10,10)
+MG.buildMap(False, map3)
+Qagent = Agent(0,0, 10*10)
+MG.setTile(Qagent.xPos, Qagent.yPos, 2);
 MG.createMapDraw(GFX)
 GFX.DrawMap(MG.cellMap)
+
+while (True):
+    while (Qagent.Move(random.choice( list(Qagent.Dirs.keys()) ), MG) == False):
+        x = 0
+
+    GFX.DrawMap(MG.cellMap)
+    GFX.update()
+
 GFX.lock()
